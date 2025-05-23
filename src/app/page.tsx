dab6dbf9d7f6c3c1880da34da4a584c117e55a9e
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2025-05-23 23:40:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
         {/* Background Image */}
         <div className="absolute inset-0 z-0">
           <Image
-            src="/images/featured-magazine.jpg"
+            src="/detailing.jpg"
             alt="Chef Salang cooking traditional Naga food"
             fill
             priority
